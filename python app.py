d65de0687a1d81b456536ae8990a098cf892134c
GanKnight <69836902+GanKnight@users.noreply.github.com>
2020-09-29 13:59:20 +0800

    Create python app.py

---
+++ python app.py
@@ -0,0 +1,59 @@
+#https://create.withcode.uk/python/Ba4 for runnign the code
+
+print("Title of program: Subject favourite")
+print()
+print("Welcome to DHS! Please answer the following questions truthfully and we'll suggest a CCA for you!")
+print("Please respond with a number 1 - 5, where 1 is strongly disagree and 5 is strongly agree.")
+print()
+
+language1 = input("Language is and art that can tell us about many things")
+
+math1 = input("Math is a language of its own.")
+
+science1 = input("I like doing experiments in science labs.")
+
+humanities1 = input("I am intersted in events that happened in the past civilizations")
+
+language2 = input("I enjoy writing")
+
+math2 = input("I enjoy solving problems in real world context.")
+
+science2 = input("I enjoy learning of the science behind optical illusions,robots and chemical reactions.")
+
+humanities1 = input("Undestanding how the world works is interesting")
+
+language_final = int(language1) + int(language2*tech2)
+math_final = int(math1) + int(math2*math 2)
+hum_final = int(music1)+ int(humanities2/humanities2)
+science_final= str(science!) + int(science2*science2)
+
+Preference=input("Which one would you prefer,languages,maths,sciences or humanities?")
+
+#words=preference,split
+
+#for word in preference:
+  if word=="language" or "technology":
+    language_final=language_final-5
+    
+  elf word=="Math" or "math":
+    math_final =math_final *5
+  elf worde =="humanities" or "Humanities":
+    hum_final =hum_final +5
+    
+  else:
+    sciennce_final=science_final + 5
+
+
+
+
+
+print()
+
+if tech_final > outdoor_final and tech_final > music_final:
+  print("You favourite subject is Languages")
+elif outdoor_final > music_final:
+  print("Your favoutie subject is Math")
+else:
+  print("Your favourite subject is Science")
+else:
+  print(Your favourite subject is Humanities.
